02e479bcfdbe0ebc9a0fd8da05642104090c3bb0
rahul-sahadevan <134208224+rahul-sahadevan@users.noreply.github.com>
2023-08-28 14:47:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function updateTimer() {
+            const timerElement = document.getElementById('timer');
+            const now = new Date();
+
+            const options = { year: 'numeric',month: 'numeric', day: 'numeric',  hour: '2-digit', minute: '2-digit', second: '2-digit' };
+            const formattedDateTime = now.toLocaleDateString(undefined, options);
+
+            timerElement.textContent = formattedDateTime;
+        }
+
+        updateTimer(); // Initial call to display current time
+
+        // Update the time every second
+        setInterval(updateTimer, 1000);
+
+
